bddcce4c512d675058f519beb4b554e8f7da1b92
mbd-s <michaelbsilverberg@gmail.com>
2016-03-20 22:13:55 -0700

    debugged

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
-
 $(document).ready(function() {
 
   var newGame = function() {
     var nextTurn = "X";
+    // keep track of clicks in case of draw
+    var clickCounter = 0;
     $("#message-box").text("It's " + nextTurn + "'s turn.");
     var winningLines = [[0, 1, 2],
                         [3, 4, 5],
@@ -12,6 +13,7 @@ $(document).ready(function() {
                         [2, 5, 8],
                         [0, 4, 8],
                         [2, 4, 6]];
+
     var checkLine = function(x, y, z) {
       // returns true if x, y, and z are the same and not null
       return ($('#box' + x).text() === $('#box' + y).text() &&
@@ -23,18 +25,23 @@ $(document).ready(function() {
       var winner = false;
       // for each array of winningLines, check to actual lines on board
       $.each(winningLines, function(i, line) {
-        if(checkLine(line[0], line[1], line[2])) {
+        if (checkLine(line[0], line[1], line[2])) {
           winner = true;
           return;
         }
       });
       return winner;
     };
+
     $(".box").on("click", function(event) {
       $(this).text(nextTurn);
+        //adds class with value of nextTurn ("x" or "o")
+      clickCounter++;
       if(somebodyWon()) {
-        $("#message-box").text(nextTurn + " won!");
+        $("#message-box").text(nextTurn + " wins!");
         $(".box").unbind("click");
+      } else if (clickCounter >= 9){
+        $("#message-box").text("Draw!");
       } else {
         if(nextTurn === "X") {
           nextTurn = "O";
@@ -42,6 +49,7 @@ $(document).ready(function() {
           nextTurn = "X";
         }
         $("#message-box").text("It's " + nextTurn + "'s turn.");
+        // stop listening for clicks
         $(this).unbind(event);
       }
     });
